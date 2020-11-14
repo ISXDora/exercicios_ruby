2d341cad3451c26a0efb6f811d198dea03c93f0f
Isadora Xavier <isinhasena@gmail.com>
2020-11-13 21:08:51 -0300

    Operador and/&&

--- operadoresLogicos.rb
+++ operadoresLogicos.rb
@@ -1,4 +1,14 @@
 puts "not true = #{ not true }"
 puts " ! true = #{ ! true }"
 
+puts "true and true = #{true and true}"
+puts "true and false = #{true and false}"
+puts "false and false = #{false and false}"
+puts "false and true = #{false and true}"
+
+puts "true && true = #{true && true}"
+puts "true && false = #{true && false}"
+puts "false && false = #{false && false}"
+puts "false && true = #{false && true}"
+
 gets 
